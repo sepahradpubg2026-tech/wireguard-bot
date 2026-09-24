168881949cfc80bdcbc788de780143080920b6c5
sepahradpubg2026-tech <sepahradpubg2026@gmail.com>
2026-09-24 17:11:48 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -463,8 +463,8 @@ def telegram_webhook(secret):
         update = telebot.types.Update.de_json(
             request.data.decode("utf-8")
         )
-bot.process_new_updates([update])
-return "OK", 200
+        bot.process_new_updates([update])
+        return "OK", 200
 
     except Exception as e:
         print("Webhook error:", e)
